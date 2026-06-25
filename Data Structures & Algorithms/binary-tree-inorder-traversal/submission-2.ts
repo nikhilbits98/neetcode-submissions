2af75b4f70469966e194a367c2ef6dcdce8740d0
Nikhil Aggarwal <nikhilbits98@gmail.com>
2026-06-25 12:53:04 +0530

    Add: binary-tree-inorder-traversal - submission-2

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-2.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    inorderTraversal(root: TreeNode | null): number[] {
+        if (root == null) {
+            return [];
+        }
+        return [
+            ...this.inorderTraversal(root.left),
+            root.val,
+            ...this.inorderTraversal(root.right),
+        ];
+    }
+}
