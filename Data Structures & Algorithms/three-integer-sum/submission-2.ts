d3329bbb2ddd7bab89a696a47b411169d6ff768a
Nikhil Aggarwal <nikhilbits98@gmail.com>
2026-07-03 14:17:59 +0530

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.ts
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums: number[]): number[][] {
+        nums = nums.sort((a,b) => a-b);
+        const n = nums.length;
+        const result = new Set<string>();
+        for(let i = 0; i<n; i++){
+            const target = nums[i] * -1;
+            let l = i+1;
+            let r = n-1;
+            while(l < r){
+                if(nums[l] + nums[r] === target){
+                    result.add(JSON.stringify([nums[i],nums[l], nums[r]]));
+                    l++;
+                    r--;
+                } else if(nums[l] + nums[r] < target){
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+        return Array.from(result, element => JSON.parse(element));
+    }
+}
